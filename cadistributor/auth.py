cd1f213702bfa43e11ac455b564880a2daa4e1f1
Ben Klein <robobenklein@gmail.com>
2020-01-29 16:17:27 -0500

    placeholder auth

---
+++ cadistributor/auth.py
@@ -0,0 +1,12 @@
+
+def has_readonly_permission(name, token):
+    # TODO
+    return False
+
+def has_worker_permission(name, token):
+    # TODO
+    return False
+
+def has_admin_permission(name, token):
+    # TODO
+    return False
